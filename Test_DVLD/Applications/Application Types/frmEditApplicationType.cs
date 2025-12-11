24272407a9f3f2aa41ccb2ec34b3de3e2c00753b
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2025-12-12 00:37:58 +0200

    create logic and UI for frmEditApplicationType and frmListApplicationType

--- Test_DVLD/Applications/Application Types/frmEditApplicationType.cs
+++ Test_DVLD/Applications/Application Types/frmEditApplicationType.cs
@@ -7,14 +7,118 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Test_DVLD.Global_Classes;
+using Test_DVLD_Buisness;
 
 namespace Test_DVLD.Applications.Application_Types
 {
     public partial class frmEditApplicationType : Form
     {
-        public frmEditApplicationType()
+
+        private int _ApplicationTypeID = -1;
+        private clsApplicationType _ApplicationType;
+
+        public frmEditApplicationType(int ApplicationTypeID)
         {
             InitializeComponent();
+
+            _ApplicationTypeID = ApplicationTypeID;
+
+        }
+
+        private void frmEditApplicationType_Load(object sender, EventArgs e)
+        {
+            lblApplicationTypeID.Text = _ApplicationTypeID.ToString();
+
+            _ApplicationType = clsApplicationType.Find(_ApplicationTypeID);
+
+            if (_ApplicationType != null)
+            {
+                txtTitle.Text = _ApplicationType.Title;
+                txtFees.Text = _ApplicationType.Fees.ToString();
+
+
+            }
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            if (!this.ValidateChildren())
+            {
+                //Here we dont continue becuase the form is not valid
+                MessageBox.Show("Some fileds are not valide!, put the mouse over the red icon(s) to see the erro", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+
+            }
+
+            _ApplicationType.Title = txtTitle.Text.Trim();
+            _ApplicationType.Fees = Convert.ToSingle(txtFees.Text.Trim());
+
+
+            if (_ApplicationType.Save())
+            {
+                MessageBox.Show("Data Saved Successfully.", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Error: Data Is not Saved Successfully.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+        private void txtTitle_Validating(object sender, CancelEventArgs e)
+        {
+
+
+            if (string.IsNullOrEmpty(txtTitle.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtTitle, "Title cannot be empty!");
+            }
+            else
+            {
+                errorProvider1.SetError(txtTitle, null);
+            }
+            ;
+
+
+        }
+
+        private void txtFees_Validating(object sender, CancelEventArgs e)
+        {
+
+
+
+            if (string.IsNullOrEmpty(txtFees.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtFees, "Fees cannot be empty!");
+                return;
+            }
+            else
+            {
+                errorProvider1.SetError(txtFees, null);
+
+            }
+            ;
+
+
+            if (!clsValidatoin.IsNumber(txtFees.Text))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtFees, "Invalid Number.");
+            }
+            else
+            {
+                errorProvider1.SetError(txtFees, null);
+            }
+            ;
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- Test_DVLD/Applications/Application Types/frmListApplicationTypes.cs
+++ Test_DVLD/Applications/Application Types/frmListApplicationTypes.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Test_DVLD_Buisness;
 
 namespace Test_DVLD.Applications.Application_Types
 {
-    public partial class frmListApplicationTypes : Form
+    public partial class frmManageApplicationTypes : Form
     {
-        public frmListApplicationTypes()
+        private DataTable _dtAllApplicationTypes;
+
+        public frmManageApplicationTypes()
         {
             InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmManageApplicationTypes_Load(object sender, EventArgs e)
+        {
+            _dtAllApplicationTypes = clsApplicationType.GetAllApplicationTypes();
+            dgvApplicationTypes.DataSource = _dtAllApplicationTypes;
+            lblRecordsCount.Text = dgvApplicationTypes.Rows.Count.ToString();
+
+            dgvApplicationTypes.Columns[0].HeaderText = "ID";
+            dgvApplicationTypes.Columns[0].Width = 110;
+
+            dgvApplicationTypes.Columns[1].HeaderText = "Title";
+            dgvApplicationTypes.Columns[1].Width = 400;
+
+            dgvApplicationTypes.Columns[2].HeaderText = "Fees";
+            dgvApplicationTypes.Columns[2].Width = 100;
+
+
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmEditApplicationType frm = new frmEditApplicationType((int)dgvApplicationTypes.CurrentRow.Cells[0].Value);
+            frm.ShowDialog();
+            frmManageApplicationTypes_Load(null, null);
+
+        }
+
+        private void btnClose_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
