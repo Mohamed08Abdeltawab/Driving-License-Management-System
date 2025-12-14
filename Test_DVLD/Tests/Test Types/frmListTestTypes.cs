71a159bc017044b980f88fa72c51061feff082ad
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2025-12-15 00:28:41 +0200

    Add classes of Testtypes in data Layer and buisness Layer and  add UI for fomrs

---
+++ Test_DVLD/Tests/Test Types/frmListTestTypes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_DVLD.Tests.Test_Types
+{
+    public partial class frmListTestTypes : Form
+    {
+        public frmListTestTypes()
+        {
+            InitializeComponent();
+        }
+
+        private void frmListTestTypes_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
