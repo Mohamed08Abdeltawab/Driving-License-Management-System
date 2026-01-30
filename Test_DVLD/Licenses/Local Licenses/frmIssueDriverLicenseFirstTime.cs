e9d41f73dab763f5506d664dac4d37fb36f45bc7
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-01-30 22:46:24 +0200

    add frmIssueDriverLicenseFirstTime ui and logic

---
+++ Test_DVLD/Licenses/Local Licenses/frmIssueDriverLicenseFirstTime.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLD_Buisness;
+using Test_DVLD.Global_Classes;
+
+namespace Test_DVLD.Licenses.Local_Licenses
+{
+    public partial class frmIssueDriverLicenseFirstTime : Form
+    {
+        private int _LocalDrivingLicenseApplicationID;
+        private clsLocalDrivingLicenseApplication _LocalDrivingLicenseApplication;
+
+        public frmIssueDriverLicenseFirstTime(int LocalDrivingLicenseApplicationID)
+        {
+            InitializeComponent();
+            _LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplicationID;
+        }
+
+        private void frmIssueDriverLicenseFirstTime_Load(object sender, EventArgs e)
+        {
+            txtNotes.Focus();
+            _LocalDrivingLicenseApplication = clsLocalDrivingLicenseApplication.FindByApplicationID(_LocalDrivingLicenseApplicationID);
+            if(_LocalDrivingLicenseApplication == null)
+            {
+                MessageBox.Show("Error loading application data.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+
+            if (!_LocalDrivingLicenseApplication.PassedAllTests())
+            {
+                MessageBox.Show("This application has not passed all required tests.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+                return;
+            }
+
+            int LicenseID = _LocalDrivingLicenseApplication.GetActiveLicenseID();
+            if(LicenseID != -1)
+            {
+                MessageBox.Show("This application already has an active license.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            ctrlDrivingLicenseApplicationInfo1.LoadApplicationInfoByApplicationID(_LocalDrivingLicenseApplicationID);
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnIssueLicense_Click(object sender, EventArgs e)
+        {
+            int LicenseID = _LocalDrivingLicenseApplication.IssueLicenseForTheFirtTime(txtNotes.Text.Trim(), clsGlobal.CurrentUser.UserID);
+            if (LicenseID != -1)
+            {
+                MessageBox.Show("License Issued Successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("License was not Issued!","Faild",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+    }
+}
