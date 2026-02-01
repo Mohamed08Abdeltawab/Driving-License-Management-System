a94bf35e40fac0896d847af7410fce3b134dec21
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-02-01 03:10:49 +0200

    add ui and logic for ctrlDriverLicenseInfo

---
+++ Test_DVLD/Licenses/Controls/ctrlDriverLicenseInfo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLD_Buisness;
+using Test_DVLD.Global_Classes;
+using Test_DVLD.Properties;
+
+namespace Test_DVLD.Licenses.Controls
+{
+    public partial class ctrlDriverLicenseInfo : UserControl
+    {
+
+        private int _LicenseID;
+        private clsLicense _License;
+        public ctrlDriverLicenseInfo()
+        {
+            InitializeComponent();
+        }
+
+        public int LicenseID
+        {
+            get
+            {
+                return _LicenseID;
+            }
+        }
+
+        public clsLicense SelectedLicenseInfo
+        {
+            get
+            {
+                return _License;
+            }
+        }
+
+        public void LoadData(int LicenseID)
+        {
+            _LicenseID = LicenseID;
+            _License = clsLicense.Find(_LicenseID);
+            if (_License == null)
+            {
+                MessageBox.Show("Error loading license data.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                _LicenseID = -1;
+                return;
+            }
+
+            lblClass.Text = _License.LicenseClassIfo.ClassName;
+            lblFullName.Text = _License.DriverInfo.PersonInfo.FullName;
+            lblLicenseID.Text = _License.LicenseID.ToString();
+            lblNationalNo.Text = _License.DriverInfo.PersonInfo.NationalNo;
+            lblGendor.Text = (_License.DriverInfo.PersonInfo.Gendor == 0 ? "Male" : "Female");
+            lblIssueDate.Text = clsFormat.DateToShort(_License.IssueDate);
+            lblIssueReason.Text = _License.IssueReasonText;
+            lblNotes.Text = (_License.Notes == "" ? "No Notes" : _License.Notes);
+            lblIsActive.Text = (_License.IsActive ? "Active" : "Inactive");
+            lblDateOfBirth.Text = clsFormat.DateToShort(_License.DriverInfo.PersonInfo.DateOfBirth);
+            lblDriverID.Text = _License.DriverInfo.DriverID.ToString();
+            lblExpirationDate.Text = clsFormat.DateToShort(_License.ExpirationDate);
+            lblIsDetained.Text = (_License.IsDetained ? "Yes" : "No");
+
+
+        }
+
+        private void _LoadPersonImage()
+        {
+            if (_License.DriverInfo.PersonInfo.Gendor == 0)
+                pbPersonImage.Image = Resources.Male_512;
+            else
+                pbPersonImage.Image = Resources.Female_512;
+
+            string ImagePath = _License.DriverInfo.PersonInfo.ImagePath;
+            if (ImagePath != "")
+            {
+                if(File.Exists(ImagePath))
+                    pbPersonImage.Load(ImagePath);
+                else
+                    MessageBox.Show("Person image file not found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
