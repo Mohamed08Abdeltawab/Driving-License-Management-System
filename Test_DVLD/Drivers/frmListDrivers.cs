ee1cdeb1d0b5d1382725c8894c8c3421c8987fe0
Mohammed A_Eltawab <ma7052@fayoum.edu.eg>
2026-02-02 18:59:17 +0200

    add ui and logic for frmListDrivers

---
+++ Test_DVLD/Drivers/frmListDrivers.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLD_Buisness;
+using Test_DVLD.People;
+
+namespace Test_DVLD.Drivers
+{
+    public partial class frmListDrivers : Form
+    {
+        public frmListDrivers()
+        {
+            InitializeComponent();
+        }
+
+        private DataTable _dtAllDrivers;
+        
+        private void frmListDrivers_Load(object sender, EventArgs e)
+        {
+            _dtAllDrivers = clsDriver.GetAllDrivers();
+            dgvDrivers.DataSource = _dtAllDrivers;
+            lblRecordsCount.Text = dgvDrivers.Rows.Count.ToString();
+
+            if(dgvDrivers.Rows.Count > 0)
+            {
+                dgvDrivers.Columns[0].HeaderText = "Driver ID";
+                dgvDrivers.Columns[0].Width = 120;
+
+                dgvDrivers.Columns[1].HeaderText = "Person ID";
+                dgvDrivers.Columns[1].Width = 120;
+
+                dgvDrivers.Columns[2].HeaderText = "National No.";
+                dgvDrivers.Columns[2].Width = 140;
+
+                dgvDrivers.Columns[3].HeaderText = "Full Name";
+                dgvDrivers.Columns[3].Width = 320;
+
+                dgvDrivers.Columns[4].HeaderText = "Date";
+                dgvDrivers.Columns[4].Width = 170;
+
+                dgvDrivers.Columns[5].HeaderText = "Active Licenses";
+                dgvDrivers.Columns[5].Width = 150;
+            }
+        }
+
+        private void cbFilterBy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtFilterValue.Enabled = (cbFilterBy.Text != "None");
+
+            if(cbFilterBy.Text == "None")
+                txtFilterValue.Enabled = false;
+            else
+                txtFilterValue.Enabled = true;
+
+            txtFilterValue.Text = "";
+            txtFilterValue.Focus();
+        }
+
+        private void txtFilterValue_TextChanged(object sender, EventArgs e)
+        {
+            string FilterColumn = "";
+            switch (cbFilterBy.Text)
+            {
+                case "Driver ID":
+                    FilterColumn = "DriverID";
+                    break;
+                case "Person ID":
+                    FilterColumn = "PersonID";
+                    break;
+                case "National No.":
+                    FilterColumn = "NationalNo";
+                    break;
+                case "Full Name":
+                    FilterColumn = "FullName";
+                    break;
+                default:
+                    FilterColumn = "None";
+                    break;
+            }
+
+            //applay filter
+            if (FilterColumn == "None" || txtFilterValue.Text.Trim() == "")
+            {
+                _dtAllDrivers.DefaultView.RowFilter = "";
+                lblRecordsCount.Text = dgvDrivers.Rows.Count.ToString();
+                return;
+            }
+
+            if (FilterColumn != "FullName" && FilterColumn != "NationalNo")
+                _dtAllDrivers.DefaultView.RowFilter = string.Format("[{0}] = {1}", FilterColumn, txtFilterValue.Text.Trim());
+            else
+                _dtAllDrivers.DefaultView.RowFilter = string.Format("[{0}] LIKE '{1}%'", FilterColumn, txtFilterValue.Text.Trim());
+
+            lblRecordsCount.Text = dgvDrivers.Rows.Count.ToString();
+        }
+
+        private void txtFilterValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (cbFilterBy.Text == "Driver ID" || cbFilterBy.Text == "Person ID")
+            {
+                e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            }
+        }
+
+        private void showDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int PeronsID = (int)dgvDrivers.CurrentRow.Cells[0].Value;
+            frmShowPersonInfo frm = new frmShowPersonInfo(PeronsID);
+            frm.ShowDialog();
+            frmListDrivers_Load(null, null);
+        }
+
+        private void issueInternationalLicenseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Not Implemented Yet");
+        }
+
+        private void showPersonLicenseHistoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int PeronsID = (int)dgvDrivers.CurrentRow.Cells[0].Value;
+            //
+        }
+    }
+}
